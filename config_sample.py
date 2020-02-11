be4bb0eb0f116284db781b3f057443fca4d7a98d
benb1028 <59970631+benb1028@users.noreply.github.com>
2020-02-11 12:17:33 -0500

    added mysql connectivity, started verification, added config

---
+++ config_sample.py
@@ -0,0 +1,1 @@
+DB = {'host':'HOST', 'port':'PORT', 'user':'USERNAME', 'passwd':'PASSWORD', 'db':'DATABASE'}
--- customer.py
+++ customer.py
@@ -1,9 +1,19 @@
+import pymysql
+
+
+
 class customerList:
     def __init__(self):
         self.data = []
         self.tempdata = {}
         self.tn = 'customers'
         self.fnl = ['fname', 'lname', 'email', 'password', 'subscribed']
+        self.errorlist = []
+        
+    def connect(self):
+        import config
+        return pymysql.connect(host=config.DB['host'], port=config.DB['port'], user=config.DB['user'], passwd=config.DB['passwd'], db=config.DB['db'], autocommit=True) #setup our credentials
+
         
     def add(self):
         self.data.append(self.tempdata)
@@ -21,12 +31,24 @@ class customerList:
             print("Could not set value at row " + str(n) + " col " + str(fn))
             
     def insert(self, n = 0):
-        cols = '","'.join(self.fnl)
-        cols = '"' + cols + '"'
+        cols = '`,`'.join(self.fnl)
+        cols = '`' + cols + '`'
         vals = ('%s,' * len(self.fnl))[:-1]
         tokens = []
         for fn in self.fnl:
             tokens.append(self.data[n][fn])
         sql = 'INSERT INTO `' + self.tn + '` (' + cols + ') VALUES (' + vals + ');'
-        print(sql)
-        print(tokens)
+        conn = self.connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        cur.execute(sql, tokens)
+        
+    def verifyNew(self, n = 0):
+        if len(self.data[n]['fname']) == 0:
+            self.errorlist.append("First name cannot be blank.")
+        # add if statements for validation of all fields
+        # add unit test
+            
+        if len(self.errorlist) < 0:
+            return False
+        else:
+            return True
